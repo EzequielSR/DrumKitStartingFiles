e7e5c7bb738d20263855b4b622301594c58706ee
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-07-01 18:15:09 -0300

    add event keypress

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
 
-
+// Botão clicado
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 // criei uma variável que seleciona os objetos com a classe chamada drum, depois soletra os números dos elementos.[0],[1]...
 
-for(var i = 0; i < numberOfDrumButtons; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case "w":
-                var crash = new Audio("sounds/crash.mp3")
-                crash.play()
-                break;
-            case "a":
-                var kickBass = new Audio("sounds/kick-bass.mp3")
-                kickBass.play()
-                break;
-            case "s":
-                var snare = new Audio("sounds/snare.mp3")
-                snare.play()
-                break;
-            case "d":
-                var tom1 = new Audio("sounds/tom-1.mp3")
-                tom1.play()
-                break;
-            case "j":
-                var tom2 = new Audio("sounds/tom-2.mp3")
-                tom2.play()
-                break;
-            case "k":
-                var tom3 = new Audio("sounds/tom-3.mp3")
-                tom3.play()
-                break;
-            case "l":
-                var tom4 = new Audio("sounds/tom-4.mp3")
-                tom4.play()
-                break;
-            
-            default:
-                break;
-        }
+        makeSound(buttonInnerHTML)
+
     })
+    //tecla clicado
+    document.addEventListener("keypress", function (event) {
+        makeSound(event.key)
+    })
+}
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3")
+            crash.play()
+            break;
+        case "a":
+            var kickBass = new Audio("sounds/kick-bass.mp3")
+            kickBass.play()
+            break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3")
+            snare.play()
+            break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3")
+            tom1.play()
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3")
+            tom2.play()
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3")
+            tom3.play()
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3")
+            tom4.play()
+            break;
+
+        default:
+            break;
+        //Eu implementei uma função chamada switch que quando a classe dos botões forem acionadas ("w,a ou j") toca o som com o respectiva imagem de fundo.
+    }
 }
 // var audio = new Audio("sounds/tom-1.mp3")
 // audio.play();
