7bb15532aae188423789df7c23975502a18eacf0
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-07-02 15:07:31 -0300

    add animation

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
+
         makeSound(buttonInnerHTML)
 
+        buttonAnimation(buttonInnerHTML)
     })
     //tecla clicado
     document.addEventListener("keypress", function (event) {
         makeSound(event.key)
+
+        buttonAnimation(event.key)
     })
 }
 function makeSound(key) {
@@ -49,6 +53,14 @@ function makeSound(key) {
             break;
         //Eu implementei uma função chamada switch que quando a classe dos botões forem acionadas ("w,a ou j") toca o som com o respectiva imagem de fundo.
     }
+}
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey)
+  activeButton.classList.add("pressed")
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed")
+  ,100})
 }
 // var audio = new Audio("sounds/tom-1.mp3")
 // audio.play();
